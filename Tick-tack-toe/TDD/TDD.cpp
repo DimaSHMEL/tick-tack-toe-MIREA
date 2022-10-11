d34ab20fdcacec3c40c155476e4509e32592f614
Dima_SHMEL <dimashmel200212@gmail.com>
2022-10-11 03:06:39 +0300

    UnitTest class created

---
+++ Tick-tack-toe/TDD/TDD.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TDD
+{
+	TEST_CLASS(TDD)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
