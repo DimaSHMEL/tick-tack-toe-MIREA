619bdc309cdcbb25c352c8971b416ad18aa2cb5c
Dima_SHMEL <dimashmel200212@gmail.com>
2022-10-12 11:24:25 +0300

    Little changes in tests + new methods

--- Tick-tack-toe/Tick-tack-toe/TickTackToe.cpp
+++ Tick-tack-toe/Tick-tack-toe/TickTackToe.cpp
@@ -2,8 +2,23 @@
 #include <exception>
 using namespace std;
 
+char** createTable()
+{
+	char** TickTackToe_Table = new char* [3];
+	for (int count = 0; count < 3; count++)
+		TickTackToe_Table[count] = new char[3];
 
-bool clearTable(char (&TickTackToe_Table)[3][3])
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			TickTackToe_Table[i][j] = '.';
+		}
+	}
+	return TickTackToe_Table;
+}
+
+bool clearTable(char **TickTackToe_Table)
 {
 	try {
 		TickTackToe_Table[0][0] =
@@ -24,13 +39,34 @@ bool clearTable(char (&TickTackToe_Table)[3][3])
 	}
 	return true;
 }
-bool writeTable(char (&TickTackToe_Table)[3][3])
+
+string writeTable(char **TickTackToe_Table)
+{
+	string answ = "";
+	try {
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				answ.append(1, TickTackToe_Table[i][j]);
+			}
+			answ.append(1, '\n');
+		}
+	}
+	catch (exception e)
+	{
+		return "TABLE IS BROKEN";
+	}
+	return answ;
+}
+
+bool fill(int x, int y, char player, char **table)
 {
 	try {
-		cout <<
-			TickTackToe_Table[0][0]<<TickTackToe_Table[0][1]<< TickTackToe_Table[0][2] << endl <<
-			TickTackToe_Table[1][0]<<TickTackToe_Table[1][1]<<TickTackToe_Table[1][2] << endl <<
-			TickTackToe_Table[2][0]<<TickTackToe_Table[2][1]<<TickTackToe_Table[2][2] << endl;
+		if(table[x][y] == '.')
+			table[x][y] = player;
+		else
+			return false;
 	}
 	catch (exception e)
 	{
@@ -38,14 +74,49 @@ bool writeTable(char (&TickTackToe_Table)[3][3])
 	}
 	return true;
 }
+
+int checkWinner(char **table)
+{
+	if ((table[0][0] == table[1][1] == table[2][2]) || (table[0][2] == table[1][1] == table[2][0]))
+	{
+		if (table[1][1] == 'X')
+			return 1;
+		else
+			return 2;
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		if (table[i][0] == table[i][1] == table[i][2])
+		{
+			if (table[i][1] == 'X')
+				return 1;
+			else
+				return 2;
+		}
+		else if (table[0][i] == table[1][i] == table[2][i])
+		{
+			if (table[0][i] == 'X')
+				return 1;
+			else
+				return 2;
+		}
+	}
+	return 0;
+}
+
+bool checkFull
+
+void game()
+{
+	char** table = createTable();
+
+	clearTable(table);
+	cout << writeTable(table);
+
+}
 int main()
 {
-	char TickTackToe_Table[3][3] = {
-		{'.', ';', '.'},
-		{'.', ';', '.'},
-		{'.', '.', '.'},
-	};
-	clearTable(TickTackToe_Table);
-	writeTable(TickTackToe_Table);
+	game();
+	
 	return 0;
 }
